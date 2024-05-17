95901ad4942fa4e48f8826b876b82c90098d936c
DIOGO DIAS MELLO <02843957222@trt8.jus.br>
2024-05-17 15:11:04 -0300

    :lipstick: style(components/Navbar/Navbar.tsx): inserindo tailwind no menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,28 @@
-import '../../static/css/navbar.css'
 import logo from '../../static/img/logo.png'
 
 const NavBar = (): JSX.Element => {
     return (
-        <header id="menu">
-            <nav className="navbar navbar-expand-lg navbar-dark">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        <img src={logo} alt="" width="50" height="50" className="d-inline-block align-text-center" />
-                        <span>Portfolio</span>
-                    </a>
-                </div>
-                <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-                    <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-                        <li className="nav-item">
-                            <a className="nav-link text-light" href="#project">Projetos</a>
-                        </li>
+        <nav className="flex items-center justify-between px-5 bg-black text-white">
+            {/* <!-- LOGO --> */}
+            <div className="logo">Green Route</div>
+
+            {/* <!-- LINKS --> */}
+            <div>
+                    {/* <!-- NAVEGAÇÃO MENU --> */}
+                    <ul className="nav-links">
+                        {/* <!-- CHECKBOX --> */}
+                        <input type="checkbox" id="checkbox_toggle" />
+                        <label htmlFor="checkbox_toggle" className="hamburger">&#9776; </label>
+                        {/* <!-- NAVEGAÇÃO MENUS --> */}
+                        <div className="menu">
+                            <li><a href="/ways/">Início</a></li>
+                            <li><a href="#">Sobre</a></li>
+                            <li><a href="#">Serviços</a></li>
+                            <li><a href="#">Contato</a></li>
+                        </div>
                     </ul>
-                </div>
-            </nav>
-        </header>
+            </div>
+        </nav>
     );
 };
 
