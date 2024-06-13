d9685ad6e67c2bfff3ea2e40a964932f27efd0aa
Diogo Dias Mello <diogoeng19@gmail.com>
2024-06-13 15:07:50 -0300

    :construction: desenvolvimento caminho para página de contato

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// PAGE
+import { Home } from './pages/home.tsx';
+import { Contact } from './pages/contact.tsx';
+
+// CSS
 import './static/css/style.css';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/contact/",
+    element: <Contact />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
