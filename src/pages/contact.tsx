5bebd389e8d710572205ccebc68d27e6b62adff3
Diogo Dias Mello <diogoeng19@gmail.com>
2024-08-05 15:29:54 -0300

    :construction: Criação do Formulário

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,17 +1,13 @@
-import dev from '../static/img/desenvolvimento.png'
-
 import { Footer } from "../components/Footer/Footer"
 import { NavBar } from "../components/Navbar/Navbar"
+import Forms from '../components/Forms/Forms'
 
 export const Contact = () => {
     return (
         <>
             <NavBar />
-            <section className="h-[calc(100vh-70px-24px)]">
-                <div className='flex flex-col w-2/4 mx-auto items-center'>
-                    <span className='bg-[#5755e2] mt-10 p-2 rounded-md font-bold'>Em Construção</span>
-                    <img className='w-3/6' src={dev} alt="pagina em contrução" title='Página em Construção'/>
-                </div>
+            <section className="h-[calc(100vh-114px-20px)]">
+                <Forms/>
             </section>
             <Footer />
         </>
