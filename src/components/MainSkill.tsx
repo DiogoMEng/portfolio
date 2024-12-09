b337f72c95e37db9b8248f6215a6a3e7c86d2051
DiogoMEng <diogoeng19@gmail.com>
2024-12-09 07:36:43 -0300

    :lipstick: feat: Visualização de Habilidades

--- src/components/MainSkill.tsx
+++ src/components/MainSkill.tsx
@@ -10,18 +10,16 @@ const MainSkill = ():JSX.Element => {
       <div className="flex gap-5 w-3/4 mx-auto mt-5 justify-center">
 
           {/* Javascript skill */}
-          <div className="bg-[#0b097e] w-1/3 p-5 rounded-md border-[1px]">
+          <div className="bg-[#0b097e] w-1/3 p-5 rounded-md border-2 duration-700 hover:border-amber-400 group">
             <div className="flex items-center">
-              <RiJavascriptFill className="text-9xl text-white" />
-              <span className="text-3xl font-bold">JavaScript</span>
+              <RiJavascriptFill className="text-9xl text-white duration-700 group-hover:text-amber-400" />
+              <span className="text-3xl font-bold duration-700 group-hover:text-amber-400">JavaScript</span>
             </div>
-            <div>
+            <div className="duration-700 group-hover:text-amber-400">
               <span className="border-b-2 font-bold text-xl">Habilidades em:</span>
               <ul className="mb-3">
                 <li className="mt-3 ml-8 list-disc">Desenvolvimento Backend</li>
-                <li className="ml-8 list-disc">Desenvolvimento Frontend</li>
-                <li className="ml-8 list-disc">Desenvolvimento Web</li>
-
+                <li className="ml-8 list-disc">Desenvolvimento Frontend Web</li>
               </ul>
               <span className="mt-5 border-b-2 font-bold text-xl">Frameworks:</span>
               <ul className="mb-3">
@@ -32,17 +30,16 @@ const MainSkill = ():JSX.Element => {
           </div>
 
           {/* TypeScript Skill */}
-          <div className="bg-[#0b097e] w-1/3 p-5 rounded-md border-[1px]">
+          <div className="bg-[#0b097e] w-1/3 p-5 rounded-md border-2 duration-700 hover:border-cyan-400 group">
             <div className="flex items-center">
-              <BiLogoTypescript className="text-9xl text-white" />
-              <span className="text-3xl font-bold">TypeScript</span>
+              <BiLogoTypescript className="text-9xl text-white duration-700 group-hover:text-cyan-400" />
+              <span className="text-3xl font-bold duration-700 group-hover:text-cyan-400">TypeScript</span>
             </div>
-            <div>
+            <div className="duration-700 group-hover:text-cyan-400">
               <span className="border-b-2 font-bold text-xl">Habilidades em:</span>
               <ul className="mb-3">
                 <li className="mt-3 ml-8 list-disc">Desenvolvimento Backend</li>
-                <li className="ml-8 list-disc">Desenvolvimento Frontend</li>
-                <li className="ml-8 list-disc">Desenvolvimento Web</li>
+                <li className="ml-8 list-disc">Desenvolvimento Frontend Web</li>
               </ul>
               <span className="mt-5 border-b-2 font-bold text-xl">Frameworks:</span>
               <ul className="mb-3">
