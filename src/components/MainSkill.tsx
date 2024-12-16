b3b9e0fc923fdea29c0c2a3e99f6435cbea05cb4
DiogoMEng <diogoeng19@gmail.com>
2024-12-16 09:09:06 -0300

    :construction: Campo de Visualização para dispositivos móveis

--- src/components/MainSkill.tsx
+++ src/components/MainSkill.tsx
@@ -2,8 +2,14 @@ import { RiJavascriptFill } from "react-icons/ri"
 import { BiLogoTypescript } from "react-icons/bi";
 import { FaGithub } from "react-icons/fa";
 import observationPosition from "../utils/observationPosition";
+import { useEffect, useRef, useState } from "react";
 
 const MainSkill = ():JSX.Element => {
+  const [isVisible, setIsVisible] = useState(false);
+  const cardRef = useRef(null);
+
+  useEffect(observationPosition(setIsVisible, cardRef), []);
+
   return (
     <section className="p-5">
       <h1 className="text-4xl text-center font-bold max-sm:text-lg">
@@ -12,21 +18,25 @@ const MainSkill = ():JSX.Element => {
       <div className="flex gap-5 w-3/4 mx-auto mt-10 justify-center max-sm:block max-sm:w-full *:max-sm:w-full">
 
           {/* Javascript skill */}
-          <div className="bg-[#0b097e] w-1/3 p-5 rounded-md border-2 duration-700 hover:border-amber-400 group max-sm:mb-5">
+          <div
+            ref={cardRef} 
+            className={
+              `bg-[#0b097e] w-1/3 p-5 rounded-md border-2 duration-700 hover:border-amber-400 group max-md:mb-5 ${isVisible ? 'max-md:border-amber-400' : ''}`
+            }
+            style={{
+              transition: 'all 0.5s ease-in-out'
+            }}
+          >
             <div className="flex items-center">
               <RiJavascriptFill className="text-9xl text-white duration-700 group-hover:text-amber-400" />
               <span className="text-4xl font-bold duration-700 group-hover:text-amber-400">JavaScript</span>
             </div>
             <div className="mt-3 duration-700 group-hover:text-amber-400">
-              <span className="border-b-2 font-bold text-2xl">Habilidades em:</span>
+              <span className="border-b-2 font-bold text-2xl">Frameworks Principais:</span>
               <ul className="mb-3 *:text-xl">
-                <li className="mt-3 ml-8 list-disc">Desenvolvimento Backend</li>
-                <li className="ml-8 list-disc">Desenvolvimento Frontend Web</li>
-              </ul>
-              <span className="mt-5 border-b-2 font-bold text-2xl">Frameworks:</span>
-              <ul className="mb-3 *:text-xl">
-                <li className="mt-3 ml-8 list-disc">NodeJS</li>
-                <li className="ml-8 list-disc">React</li>
+                <li className="mt-3 ml-8 list-disc">Node.js</li>
+                <li className="ml-8 list-disc">Express.js</li>
+                <li className="ml-8 list-disc">NestJS</li>
               </ul>
             </div>
             <div className="bg-slate-100 w-32 mt-5 px-4 py-2 rounded-md text-xl items-center duration-700 group-hover:bg-amber-400">
@@ -40,16 +50,12 @@ const MainSkill = ():JSX.Element => {
               <BiLogoTypescript className="text-9xl text-white duration-700 group-hover:text-cyan-400" />
               <span className="text-4xl font-bold duration-700 group-hover:text-cyan-400">TypeScript</span>
             </div>
-            <div className="duration-700 group-hover:text-cyan-400">
-              <span className="border-b-2 font-bold text-2xl">Habilidades em:</span>
+            <div className="mt-3 duration-700 group-hover:text-cyan-400">
+              <span className="border-b-2 font-bold text-2xl">Frameworks Principais:</span>
               <ul className="mb-3 *:text-xl">
-                <li className="mt-3 ml-8 list-disc">Desenvolvimento Backend</li>
-                <li className="ml-8 list-disc">Desenvolvimento Frontend Web</li>
-              </ul>
-              <span className="mt-5 border-b-2 font-bold text-2xl">Frameworks:</span>
-              <ul className="mb-3 *:text-xl">
-                <li className="mt-3 ml-8 list-disc">NodeJS</li>
-                <li className="ml-8 list-disc">React</li>
+                <li className="mt-3 ml-8 list-disc">Node.js</li>
+                <li className="ml-8 list-disc">Express.js</li>
+                <li className="ml-8 list-disc">NestJS</li>
               </ul>
             </div>
             <div className="bg-slate-100 w-32 mt-5 px-4 py-2 rounded-md text-xl items-center duration-700 group-hover:bg-cyan-400">
