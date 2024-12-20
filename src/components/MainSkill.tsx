4e70e0ad85c33e69ba2678e6de55c8e39b59910b
DiogoMEng <diogoeng19@gmail.com>
2024-12-20 09:23:48 -0300

    :construction: Desenvolvendo componente de habilidades

--- src/components/MainSkill.tsx
+++ src/components/MainSkill.tsx
@@ -1,14 +1,14 @@
 import { RiJavascriptFill } from "react-icons/ri"
 import { BiLogoTypescript } from "react-icons/bi";
 import { FaGithub } from "react-icons/fa";
-import observationPosition from "../../utils/observationPosition";
+import observationPosition from "../utils/observationPosition";
 import { useEffect, useRef, useState } from "react";
 
 const MainSkill = ():JSX.Element => {
   const [isVisible, setIsVisible] = useState(false);
   const cardRef = useRef(null);
 
-  useEffect(observationPosition(setIsVisible, cardRef, 0.3), []);
+  useEffect(observationPosition(setIsVisible, cardRef, 0.5), []);
 
   return (
     <section className="p-5">
@@ -18,7 +18,7 @@ const MainSkill = ():JSX.Element => {
       <div className="flex gap-5 w-3/4 mx-auto mt-10 justify-center max-sm:block max-sm:w-full *:max-sm:w-full">
 
           {/* Javascript skill */}
-          {/* <div
+          <div
             ref={cardRef}
             className={
               `bg-[#0b097e] w-1/3 p-5 rounded-md border-2 duration-700 hover:border-amber-400 *:hover:text-amber-400 group max-md:mb-5 ${isVisible ? 'max-md:border-amber-400 max-md:*:text-amber-400' : ''}`
@@ -38,16 +38,16 @@ const MainSkill = ():JSX.Element => {
                 <li className="ml-8 list-disc">Express.js</li>
                 <li className="ml-8 list-disc">NestJS</li>
               </ul>
-            </div> */}
+            </div>
 
             {/* button - redireciona para projetos */}
-            {/* <div className={`bg-slate-100 w-32 mt-5 px-4 py-2 rounded-md text-xl items-center duration-700 group-hover:bg-amber-400 ${isVisible ? 'max-md:bg-amber-400' : ''}`}>
+            <div className={`bg-slate-100 w-32 mt-5 px-4 py-2 rounded-md text-xl items-center duration-700 group-hover:bg-amber-400 ${isVisible ? 'max-md:bg-amber-400' : ''}`}>
               <a className="flex text-[#0b097e]" href="https://github.com/DiogoMEng/projetos-javascript">Github <FaGithub className={`ml-3 text-[#0b097e] text-2xl group-hover:animate-bounce ${isVisible ? 'animate-bounce' : ''}`}/></a>
             </div>
-          </div> */}
+          </div>
 
           {/* TypeScript Skill */}
-          {/* <div
+          <div
             ref={cardRef} 
             className={
               `bg-[#0b097e] w-1/3 p-5 rounded-md border-2 duration-700 hover:border-cyan-400 *:hover:text-cyan-400 group max-md:mb-5 ${isVisible ? 'max-md:border-cyan-400 max-md:*:text-cyan-400' : ''}`
@@ -67,13 +67,13 @@ const MainSkill = ():JSX.Element => {
                 <li className="ml-8 list-disc">Express.js</li>
                 <li className="ml-8 list-disc">NestJS</li>
               </ul>
-            </div> */}
+            </div>
 
             {/* button - redireciona para projetos */}
-            {/* <div className={`bg-slate-100 w-32 mt-5 px-4 py-2 rounded-md text-xl items-center duration-700 group-hover:bg-cyan-400 ${isVisible ? 'max-md:bg-cyan-400' : ''}`}>
+            <div className={`bg-slate-100 w-32 mt-5 px-4 py-2 rounded-md text-xl items-center duration-700 group-hover:bg-cyan-400 ${isVisible ? 'max-md:bg-cyan-400' : ''}`}>
               <a className="flex text-[#0b097e]" href="https://github.com/DiogoMEng/projetos-javascript">Github <FaGithub className={`ml-3 text-[#0b097e] text-2xl group-hover:animate-bounce ${isVisible ? 'animate-bounce' : ''}`}/></a>
             </div>
-          </div> */}
+          </div>
       </div>
     </section>
   )
